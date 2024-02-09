7ea699515e7bf1d77e0950f058aa3f01411d8190
damilola <adamidogo@gmail.com>
2024-02-09 14:07:09 +0100

    update

--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -12,12 +12,92 @@ class _onboardState extends State<onboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Onboardcontent(
-          image: 'images/onb1.png',
-          description: 'test',
-          title: 'looo',
-        ),
+      appBar: AppBar(
+        title: Center(
+            child: Text(
+          "LifeLine",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        )),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: PageView.builder(
+              itemBuilder: (context, index) => Onboardcontent(
+                image: 'images/onb2.png',
+                description:
+                    'Send out a distress signal with just a tap, and get matched with nearby, qualified volunteers who can offer assistance until professional help arrives. Every second counts in an emergency.',
+                title: 'Help Arrives Faster',
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 120,
+            child: Row(
+              children: [
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Container(
+                    height: 65,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      color: Colors.white,
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        // Add your button action here
+                      },
+                      child: Text(
+                        "Skip",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 221, 12, 12),
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                  child: Container(
+                    height: 65,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 2,
+                          blurRadius: 5,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                      color: Color.fromARGB(255, 221, 12, 12),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        // Add your button action here
+                      },
+                      child: Text(
+                        "Next",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
@@ -36,21 +116,12 @@ class Onboardcontent extends StatelessWidget {
     return Column(
       children: [
         Spacer(),
-        Stack(
-          children: [
-            SvgPicture.asset(
-              "images/onbg.svg",
-              width: MediaQuery.of(context).size.width,
-            ),
-            Positioned(
-              top: 90,
-              child: Image.asset(
-                image,
-                height: 250,
-                width: MediaQuery.of(context).size.width,
-              ),
-            ),
-          ],
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Image.asset(
+            image,
+            fit: BoxFit.fitWidth,
+          ),
         ),
         Spacer(),
         Text(
@@ -59,7 +130,7 @@ class Onboardcontent extends StatelessWidget {
           style: Theme.of(context)
               .textTheme
               .headline5!
-              .copyWith(fontWeight: FontWeight.w500),
+              .copyWith(fontWeight: FontWeight.bold),
         ),
         SizedBox(height: 16),
         Text(
