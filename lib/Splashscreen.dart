273920ab0082ac82616c21127cc58acb9ddca854
damilola <adamidogo@gmail.com>
2024-02-09 17:22:24 +0100

    Update

--- lib/Splashscreen.dart
+++ lib/Splashscreen.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:lifeline/onboarding.dart';
+import 'package:lottie/lottie.dart';
 
-class Splashscreen extends StatelessWidget {
+class Splashscreen extends StatefulWidget {
   const Splashscreen({super.key});
+
+  @override
+  State<Splashscreen> createState() => _SplashscreenState();
+}
+
+class _SplashscreenState extends State<Splashscreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    // Introducing a 3-second delay before navigating to the main screen
+    Future.delayed(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => onboard()),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0x1A1A1A),
-      body: Center(child: Image(image: AssetImage("images/logo.png"))),
+      body: Column(
+        children: [
+          Expanded(
+              child:
+                  Center(child: Image(image: AssetImage("images/logo.png")))),
+          Lottie.asset('images/loadanimation.json', width: 100),
+        ],
+      ),
     );
   }
 }
--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -9,26 +9,67 @@ class onboard extends StatefulWidget {
 }
 
 class _onboardState extends State<onboard> {
+  late PageController _pageController;
+  int _pageindex = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(
-            child: Text(
-          "LifeLine",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        )),
-      ),
+      backgroundColor: Color.fromARGB(239, 243, 246, 249),
       body: Column(
         children: [
+          SizedBox(
+            height: 50,
+          ),
+          Center(
+              child: Text(
+            "LifeLine",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          )),
           Expanded(
             child: PageView.builder(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _pageindex = index;
+                });
+              },
+              itemCount: data.length,
               itemBuilder: (context, index) => Onboardcontent(
-                image: 'images/onb2.png',
-                description:
-                    'Send out a distress signal with just a tap, and get matched with nearby, qualified volunteers who can offer assistance until professional help arrives. Every second counts in an emergency.',
-                title: 'Help Arrives Faster',
+                image: data[index].image,
+                description: data[index].description,
+                title: data[index].title,
               ),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ...List.generate(
+                    data.length,
+                    (index) => Padding(
+                          padding: const EdgeInsets.only(right: 4),
+                          child: Dotindicator(
+                            isActive: index == _pageindex,
+                          ),
+                        )),
+              ],
             ),
           ),
           SizedBox(
@@ -43,7 +84,7 @@ class _onboardState extends State<onboard> {
                     height: 65,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(15),
-                      color: Colors.white,
+                      color: Colors.transparent,
                     ),
                     child: TextButton(
                       onPressed: () {
@@ -79,7 +120,17 @@ class _onboardState extends State<onboard> {
                     ),
                     child: TextButton(
                       onPressed: () {
+                        print(_pageController.page);
                         // Add your button action here
+                        _pageController.nextPage(
+                            duration: Duration(milliseconds: 300),
+                            curve: Curves.ease);
+                        if (_pageindex == data.length - 1) {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => Home()),
+                          );
+                        }
                       },
                       child: Text(
                         "Next",
@@ -102,6 +153,48 @@ class _onboardState extends State<onboard> {
     );
   }
 }
+
+class Dotindicator extends StatelessWidget {
+  const Dotindicator({super.key, this.isActive = false});
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: isActive ? 7 : 6,
+      width: 6,
+      decoration: BoxDecoration(
+          color: isActive ? Colors.red : Colors.red.withOpacity(0.4),
+          borderRadius: BorderRadius.circular(12)),
+    );
+  }
+}
+
+class Onboardtab {
+  final String image, title, description;
+  Onboardtab(
+      {required this.title, required this.image, required this.description});
+}
+
+final List<Onboardtab> data = [
+  Onboardtab(
+    image: 'images/onb2.png',
+    description:
+        'Send out a distress signal with just a tap, and get matched with nearby, qualified volunteers who can offer assistance until professional help arrives. Every second counts in an emergency.',
+    title: 'Help Arrives Faster',
+  ),
+  Onboardtab(
+    image: 'images/onb1.png',
+    description:
+        'Download the app and connect with trained volunteers ready to help in emergencies. Together, we make our community stronger.',
+    title: 'Ready to be a hero?',
+  ),
+  Onboardtab(
+    image: 'images/onb3.png',
+    description:
+        'Connect with neighbors, learn from experts, and build a network of support for those in need.',
+    title: 'More than just an app, a community',
+  )
+];
 
 class Onboardcontent extends StatelessWidget {
   const Onboardcontent({
@@ -133,9 +226,12 @@ class Onboardcontent extends StatelessWidget {
               .copyWith(fontWeight: FontWeight.bold),
         ),
         SizedBox(height: 16),
-        Text(
-          description,
-          textAlign: TextAlign.center,
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: Text(
+            description,
+            textAlign: TextAlign.center,
+          ),
         ),
         Spacer()
       ],
