f4bb350957b08d4094fb0b2c39dcecf57fddf68d
damilola <adamidogo@gmail.com>
2024-02-11 07:29:56 +0100

    Accountsetup Ui update

--- lib/Accountsetup.dart
+++ lib/Accountsetup.dart
@@ -1,6 +1,7 @@
 import 'package:date_format_field/date_format_field.dart';
 import 'package:flutter/material.dart';
 import 'package:lifeline/auth.dart';
+import 'package:page_transition/page_transition.dart';
 
 class Accountsetup extends StatefulWidget {
   const Accountsetup({super.key});
@@ -12,6 +13,24 @@ class Accountsetup extends StatefulWidget {
 class _AccountsetupState extends State<Accountsetup> {
   final fname = TextEditingController();
   final lname = TextEditingController();
+  final DOB = TextEditingController();
+  int _pageindex = 0;
+  late PageController _pageController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    _pageController = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     DateTime? date;
@@ -32,9 +51,87 @@ class _AccountsetupState extends State<Accountsetup> {
           ),
           Expanded(
             child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                setState(() {
+                  _pageindex = index;
+                });
+              },
               children: [
-                Page1(fname: fname, lname: lname),
-                Page1(fname: fname, lname: lname)
+                Page1(
+                  fname: fname,
+                  lname: lname,
+                  DOB: DOB,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 18),
+                  child: ListView(
+                    children: [],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 100,
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    ...List.generate(
+                        2,
+                        (index) => Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.only(right: 4),
+                                child: Dotindicator(
+                                  isActive: index == _pageindex,
+                                ),
+                              ),
+                            ))
+                  ],
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                  height: 65,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 5,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                    color: Color.fromARGB(255, 221, 12, 12),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      print(_pageController.page);
+                      // Add your button action here
+                      _pageController.nextPage(
+                          duration: Duration(milliseconds: 300),
+                          curve: Curves.ease);
+                      if (_pageindex == 2) {
+                        Navigator.push(
+                            context,
+                            PageTransition(
+                                type: PageTransitionType.leftToRight,
+                                child: Signup()));
+                      }
+                    },
+                    child: Text(
+                      "Next",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           )
@@ -47,11 +144,12 @@ class Page1 extends StatelessWidget {
     super.key,
     required this.fname,
     required this.lname,
+    required this.DOB,
   });
 
   final TextEditingController fname;
   final TextEditingController lname;
-
+  final TextEditingController DOB;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -124,6 +222,7 @@ class Page1 extends StatelessWidget {
               }
             }
           },
+          controller: DOB,
           type: DateFormatType.type2,
           addCalendar: true,
           decoration: InputDecoration(
@@ -143,6 +242,20 @@ class Page1 extends StatelessWidget {
           lastDate: DateTime.now(),
         )
       ]),
+    );
+  }
+}
+
+class Dotindicator extends StatelessWidget {
+  const Dotindicator({super.key, this.isActive = false});
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 8,
+      decoration: BoxDecoration(
+          color: isActive ? Colors.red : Colors.red.withOpacity(0.4),
+          borderRadius: BorderRadius.circular(12)),
     );
   }
 }
